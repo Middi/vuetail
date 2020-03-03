8d673b0c4a0c5a1beea3fe5dc1d34a12a1409bd4
richardmiddleton <richardmiddleton@numiko.com>
2020-03-03 09:39:40 +0000

    first commit

---
+++ backend/news/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from wagtail.api import APIField
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.api.fields import ImageRenditionField
+
+class NewsPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+        ImageChooserPanel('image'),
+    ]
+    api_fields = [
+        APIField('intro'),
+        APIField('body'),
+        APIField('image_thumbnail', serializer=ImageRenditionField('fill-100x100', source='image')),
+    ]
---
+++ frontend/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <ul id="example-1">
+      <li v-for="item in news" :key="item.id">
+        <h2 class="text-3xl">{{ item.title }}</h2>
+        <p class="mt-20" v-html="item.body"></p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+import './assets/styles/index.css';
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data () {
+      return {
+          news: []
+      }
+  },
+  mounted () {
+      axios
+      .get('http://localhost:8000/api/v2/pages/?type=news.NewsPage&fields=intro,body')
+      .then(response => (this.news = response.data.items))
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
